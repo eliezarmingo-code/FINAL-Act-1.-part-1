74bc120eb39b161e88ac2a5b1b34cb4797990d94
Eliezar Mingo <eliezarmingo@gmail.com>
2026-04-09 09:03:16 +0800

    Add files via upload

---
+++ FINAL act.py
@@ -0,0 +1,22 @@
+print("Part 1: Identify and Create (Warm-up)")
+
+# A tuple of my five favorite fruits
+fruits = ("apple", "banana", "orange", "grape", "Mango")
+print(" My Five Favorite fruits are: ", fruits)
+
+# List of my daily tasks
+tasks = ["wake up", "eat breakfast", "go to work", "exercise", "sleep"]
+print(" My Daily Tasks are: ", tasks)
+
+# A set of unique numbers from this list: [1, 2, 2, 3, 4, 4, 5]
+numbers = [1, 2, 2, 3, 4, 4, 5]
+unique_numbers = set(numbers)
+print(" My Unique Numbers are: ", unique_numbers)
+
+# A dictionary with keys: "name", "age", "course"
+me = {
+    "name": "Eliezar Mingo",
+    "age": 22,
+    "course": "Information Technology"
+}
+print(" My Personal Information is: ", me)
